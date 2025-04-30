a6d49bc6a59f6f5bc6e3b53d364d26111eb2d90e
matth <matthew.thomas690@gmail.com>
2025-04-30 21:51:00 +0100

    added theme

--- app/src/main/java/com/kellycasey/womeninstem/ui/theme/Color.kt
+++ app/src/main/java/com/kellycasey/womeninstem/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.kellycasey.womeninstem.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary – Pastel Pink
+val PrimaryPink           = Color(0xFFFFDDE1)
+val PrimaryPinkVariant    = Color(0xFFF2C5D4)
+val OnPrimary             = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary – Dusty Mauve
+val SecondaryMauve        = Color(0xFFA56CC1)
+val SecondaryMauveVariant = Color(0xFF8E57A8)
+val OnSecondary           = Color(0xFFFFFFFF)
+
+// Neutral – Charcoal Grey
+val CharcoalGrey          = Color(0xFF333333)
+val OnBackground          = Color(0xFF333333)
+
+// Background & Surface
+val BackgroundWhite       = Color(0xFFFFFFFF)
+val SurfaceWhite          = Color(0xFFFFFFFF)
